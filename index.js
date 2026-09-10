237c220f7d14e75b068e455d4ef9568b0d0310b2
prabhatbhatiaa <prabhatbhatia09@gmail.com>
2026-09-10 13:10:52 +0530

    Added a simple /about endpoint to understand requests and responses

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/about',(req,res) => {
+    res.send('About Page')
 })
 
 app.listen(port, () => {
